ff9feb148d57a681f2bc73b9aec9ff65d467eeb8
Brako Sanchez <brakoresanchez@gmail.com>
2025-05-13 12:47:24 -0600

    fix: remove problematic redirects configuration

--- next.config.js
+++ next.config.js
@@ -9,15 +9,6 @@ const nextConfig = {
   images: {
     domains: ['www.tacosborabora.com', 'tacos-bora-bora.vercel.app']
   },
-  async redirects() {
-    return [
-      {
-        source: '/ordenar',
-        destination: '/ordenar',
-        permanent: true,
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
