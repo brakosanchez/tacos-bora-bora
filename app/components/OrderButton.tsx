31ad0e3c177903593c9d9295e9d95f71967f7bd8
Brako Sanchez <brakoresanchez@gmail.com>
2025-05-13 18:13:17 -0600

    fix: update OrderButton to use router.push()

--- app/components/OrderButton.tsx
+++ app/components/OrderButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 interface OrderButtonProps {
   className?: string;
@@ -9,15 +9,19 @@ interface OrderButtonProps {
 }
 
 const OrderButton: React.FC<OrderButtonProps> = ({ className = '', onClick }) => {
+  const router = useRouter();
+
   const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
     if (onClick) {
       onClick();
     }
+    router.push('/ordenar');
   };
 
   return (
-    <Link
-      href="/ordenar"
+    <button
+      type="button"
       onClick={handleClick}
       className={`font-unbounded bg-gradient-to-r from-bora-red to-bora-orange 
                  px-8 py-3 rounded-lg text-xl shadow-lg 
@@ -26,7 +30,7 @@ const OrderButton: React.FC<OrderButtonProps> = ({ className = '', onClick }) =>
                  hover:shadow-bora-orange/50 hover:shadow-2xl ${className}`}
     >
       Ordena Ahora!
-    </Link>
+    </button>
   );
 };
 
