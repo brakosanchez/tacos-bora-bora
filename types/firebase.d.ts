cb20d296acf721b6a248f416d76e502be6a82d8d
Brako Sanchez <brakoresanchez@gmail.com>
2025-05-13 21:47:35 -0600

    refactor: enhance firebase type declarations

--- types/firebase.d.ts
+++ types/firebase.d.ts
@@ -1,7 +1,22 @@
 import { Auth } from 'firebase/auth';
 import { Firestore } from 'firebase/firestore';
+import { FirebaseApp } from 'firebase/app';
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_FIREBASE_API_KEY: string;
+      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+      NEXT_PUBLIC_FIREBASE_APP_ID: string;
+    }
+  }
+}
 
 declare module '@/lib/firebase' {
+  export const app: FirebaseApp;
   export const auth: Auth;
   export const firestore: Firestore;
 }
