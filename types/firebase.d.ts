4eecc4af300ebe64b72b4c7afc5afdfc5dc0d130
Brako Sanchez <brakoresanchez@gmail.com>
2025-05-13 21:50:36 -0600

    fix: change firebase import to relative path

--- types/firebase.d.ts
+++ types/firebase.d.ts
@@ -1,22 +1,11 @@
-import { Auth } from 'firebase/auth';
-import { Firestore } from 'firebase/firestore';
-import { FirebaseApp } from 'firebase/app';
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      NEXT_PUBLIC_FIREBASE_API_KEY: string;
-      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
-      NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
-      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
-      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
-      NEXT_PUBLIC_FIREBASE_APP_ID: string;
-    }
-  }
-}
-
 declare module '@/lib/firebase' {
-  export const app: FirebaseApp;
-  export const auth: Auth;
-  export const firestore: Firestore;
+  import { Auth } from 'firebase/auth';
+  import { Firestore } from 'firebase/firestore';
+  import { FirebaseApp } from 'firebase/app';
+
+  const app: FirebaseApp;
+  const auth: Auth;
+  const firestore: Firestore;
+
+  export { app, auth, firestore };
 }
