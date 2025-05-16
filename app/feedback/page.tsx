1b8931dcea1dcae1535d862949f21244572e42f2
Brako Sanchez <brakoresanchez@gmail.com>
2025-05-15 23:57:48 -0600

    fix: update heroicons import structure
    - Change ArrowRightIcon import to use @heroicons/react/24/outline
    - Add @heroicons/react/24 package to dependencies
    - Fix import error in feedback page

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { ArrowRightIcon } from '@heroicons/react';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import SuccessModal from '@/components/SuccessModal';
 
 const questions = [
